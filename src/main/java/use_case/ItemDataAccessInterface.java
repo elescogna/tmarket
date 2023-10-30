2cd7b6f77152735e4dce429a69e64a9f3187e762
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2023-10-30 16:41:31 -0400

    fix(item-data-access-object): made ItemDataAccessObject into furniture instead

--- src/main/java/use_case/ItemDataAccessInterface.java
+++
@@ -1,8 +0,0 @@
-package use_case;
-
-import java.io.IOException;
-
-public interface ItemDataAccessInterface {
-    // TODO obviously change this to an ArrayList or something
-    String getAllItems() throws IOException;
-}
