2a9f9e4bfb58ad2e6e77ca83a8f3883c54c47dbd
elescogna <eleonorascognamiglio03@gmail.com>
2023-10-31 10:20:54 -0400

    New entity: implemented Clothing

---
+++ src/main/java/entities/Clothing.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class Clothing {
+    private String brand;
+    private String colour;
+    private String size;
+    private String material;
+
+    public void setBrand(String brand) { this.brand = brand; }
+    public String getBrand() { return this.brand; }
+    public void setColour(String colour) { this.colour = colour; }
+    public String getColour() { return this.colour; }
+    public void setSize(String size) { this.size = size; }
+    public String getSize() { return this.size; }
+    public void setMaterial(String material) { this.material = material; }
+    public String getMaterial() { return this.material; }
+}
