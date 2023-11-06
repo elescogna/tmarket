6bc1238d6726fede76a04b4aeaa46aa9c81e8f66
Aina Merchant <ainamerchant10@gmail.com>
2023-11-06 13:56:47 -0500

    "Home Use Case - changed data type for ArrayList<Item> name"

--- src/main/java/interface_adapter/home/HomeState.java
+++ src/main/java/interface_adapter/home/HomeState.java
@@ -1,11 +1,13 @@
 package interface_adapter.home;
+import entities.Item;
+
 import java.util.ArrayList;
 public class HomeState {
-    private ArrayList<String> wantedPosts = new ArrayList<>();
-    public ArrayList<String> getWantedPosts() {
+    private ArrayList<Item> wantedPosts = new ArrayList<>();
+    public ArrayList<Item> getWantedPosts() {
         return this.wantedPosts;
     }
-    public void setWantedPosts(ArrayList<String> wantedPosts) {
+    public void setWantedPosts(ArrayList<Item> wantedPosts) {
         this.wantedPosts = wantedPosts;
     }
 }
