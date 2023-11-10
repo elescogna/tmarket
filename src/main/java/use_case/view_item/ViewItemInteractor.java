5d94cae107b68b9f68458d7b51e8855159968aeb
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2023-11-10 02:10:39 -0500

    feat(view-item): implemented ViewItemInteractor

--- src/main/java/use_case/view_item/ViewItemInteractor.java
+++ src/main/java/use_case/view_item/ViewItemInteractor.java
@@ -1,28 +1,53 @@
 package use_case.view_item;
 
+import entities.Item;
+import java.io.IOException;
+
 public class ViewItemInteractor implements ViewItemInputBoundary {
-    final ViewItemDataAccessInterface clothingDataAccessObject;
-    final ViewItemDataAccessInterface furnitureDataAccessObject;
-    final ViewItemDataAccessInterface orderDataAccessObject;
-    final ViewItemDataAccessInterface schoolItemDataAccessObject;
-    final ViewItemDataAccessInterface technologyDataAccessObject;
-    final ViewItemOutputBoundary homePresenter;
+  final ViewItemDataAccessInterface clothingDataAccessObject;
+  final ViewItemDataAccessInterface furnitureDataAccessObject;
+  final ViewItemDataAccessInterface schoolItemDataAccessObject;
+  final ViewItemDataAccessInterface technologyDataAccessObject;
+  final ViewItemOutputBoundary viewItemPresenter;
 
-    public ViewItemInteractor(
-            ViewItemDataAccessInterface clothingDataAccessObject,
-            ViewItemDataAccessInterface furnitureDataAccessObject,
-            ViewItemDataAccessInterface orderDataAccessObject,
-            ViewItemDataAccessInterface schoolItemDataAccessObject,
-            ViewItemDataAccessInterface technologyDataAccessObject,
-            ViewItemOutputBoundary homeOutputBoundary) {
-        this.clothingDataAccessObject = clothingDataAccessObject;
-        this.furnitureDataAccessObject = furnitureDataAccessObject;
-        this.orderDataAccessObject = orderDataAccessObject;
-        this.schoolItemDataAccessObject = schoolItemDataAccessObject;
-        this.technologyDataAccessObject = technologyDataAccessObject;
-        this.homePresenter = homeOutputBoundary;
-            }
+  public ViewItemInteractor(
+      ViewItemDataAccessInterface clothingDataAccessObject,
+      ViewItemDataAccessInterface furnitureDataAccessObject,
+      ViewItemDataAccessInterface orderDataAccessObject,
+      ViewItemDataAccessInterface schoolItemDataAccessObject,
+      ViewItemDataAccessInterface technologyDataAccessObject,
+      ViewItemOutputBoundary viewItemOutputBoundary) {
+    this.clothingDataAccessObject = clothingDataAccessObject;
+    this.furnitureDataAccessObject = furnitureDataAccessObject;
+    this.schoolItemDataAccessObject = schoolItemDataAccessObject;
+    this.technologyDataAccessObject = technologyDataAccessObject;
+    this.viewItemPresenter = viewItemOutputBoundary;
+  }
 
-    @Override
-    public void execute(ViewItemInputData viewItemData) {}
+  @Override
+  public void execute(ViewItemInputData viewItemData) {
+    try {
+      Item itemToDisplay;
+
+      // try to find the item in any of the collections
+      String itemIdToGet = viewItemData.getItemId();
+
+      if ((itemToDisplay = clothingDataAccessObject.getItem(itemIdToGet)) !=
+              null ||
+          (itemToDisplay = furnitureDataAccessObject.getItem(itemIdToGet)) !=
+              null ||
+          (itemToDisplay = schoolItemDataAccessObject.getItem(itemIdToGet)) !=
+              null ||
+          (itemToDisplay = technologyDataAccessObject.getItem(itemIdToGet)) !=
+              null) {
+        viewItemPresenter.prepareSuccessView(
+            new ViewItemOutputData(itemToDisplay));
+      } else { // if the item wasn't found in any collection
+        throw new IOException("Item with the given ID not found in database.");
+      }
+    } catch (IOException e) {
+      viewItemPresenter.prepareFailView(
+          "No such item was found in the database.");
+    }
+  }
 }
