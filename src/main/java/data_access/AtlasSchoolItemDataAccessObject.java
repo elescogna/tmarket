f3654aa8c908bb95e6ea6c46d661b038824a68a4
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2023-11-10 01:50:03 -0500

    feat(view-item): implemented methods in ViewItemDataAccessInterface for all Item DAOs

--- src/main/java/data_access/AtlasSchoolItemDataAccessObject.java
+++ src/main/java/data_access/AtlasSchoolItemDataAccessObject.java
@@ -13,9 +13,10 @@ import okhttp3.Response;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import use_case.home.HomeDataAccessInterface;
+import use_case.view_item.ViewItemDataAccessInterface;
 
-public class AtlasSchoolItemDataAccessObject
-    extends AtlasDataAccessObject implements HomeDataAccessInterface {
+public class AtlasSchoolItemDataAccessObject extends AtlasDataAccessObject
+    implements HomeDataAccessInterface, ViewItemDataAccessInterface {
     private static final String atlasCollectionName = "school-items";
 
     @Override
@@ -73,6 +74,64 @@ public class AtlasSchoolItemDataAccessObject
             }
 
             return result;
+        }
+    }
+
+    @Override
+    public Item getItem(String idToGet) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        HashMap<String, Object> requestBodyMap = new HashMap<String, Object>();
+
+        requestBodyMap.put("dataSource", atlasDataSourceName);
+        requestBodyMap.put("database", atlasDatabaseName);
+        requestBodyMap.put("collection", atlasCollectionName);
+
+        HashMap<String, String> filter = new HashMap<String, String>();
+        filter.put("_id", idToGet);
+
+        requestBodyMap.put("filter", new HashMap<String, String>());
+
+        Request request = preparePostRequest(atlasCollectionName, "/action/findOne",
+                requestBodyMap);
+
+        try (Response response = client.newCall(request).execute()) {
+            JSONObject responseJson = new JSONObject(response.body().string());
+            try {
+                JSONObject itemDocument = responseJson.getJSONObject("document");
+                // General item attributes
+
+                String id = itemDocument.getString("_id");
+                String name = itemDocument.getString("name");
+                String description = itemDocument.getString("description");
+                String condition = itemDocument.getString("condition");
+                double price = itemDocument.getDouble("price");
+                int age = itemDocument.getInt("age");
+                boolean soldYet = itemDocument.getBoolean("soldYet");
+                String pickupAddress = itemDocument.getString("pickupAddress");
+                double radius = itemDocument.getDouble("radius");
+                // TODO: when we get around to this, we have to get a student based on
+                // the owner ID that is provided here like:
+                // studentDataAccessObject.get(jsonDocument.getString("ownerId"));
+                Student owner = new Student("id", "test", "test", "test", "test", false,
+                        new ArrayList<>());
+                String type = itemDocument.getString("type");
+                String picture = itemDocument.getString("picture");
+                LocalDateTime creationTime =
+                    LocalDateTime.parse(itemDocument.getString("creationTime"));
+
+                // Item-specific attributes
+                String brand = itemDocument.getString("brand");
+                String colour = itemDocument.getString("colour");
+
+                SchoolItem newItem =
+                    new SchoolItem(id, name, description, condition, price, age,
+                            soldYet, pickupAddress, radius, owner, type, picture,
+                            creationTime, brand, colour);
+
+                return newItem;
+            } catch (JSONException e) { // this means it's null
+                return null;
+            }
         }
     }
 }
--- src/main/java/data_access/AtlasTechnologyDataAccessObject.java
+++ src/main/java/data_access/AtlasTechnologyDataAccessObject.java
@@ -11,11 +11,13 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import use_case.home.HomeDataAccessInterface;
+import use_case.view_item.ViewItemDataAccessInterface;
 
-public class AtlasTechnologyDataAccessObject
-    extends AtlasDataAccessObject implements HomeDataAccessInterface {
+public class AtlasTechnologyDataAccessObject extends AtlasDataAccessObject
+    implements HomeDataAccessInterface, ViewItemDataAccessInterface {
     private static final String atlasCollectionName = "technology";
 
     @Override
@@ -80,6 +82,70 @@ public class AtlasTechnologyDataAccessObject
             }
 
             return result;
+        }
+    }
+
+    @Override
+    public Item getItem(String idToGet) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        HashMap<String, Object> requestBodyMap = new HashMap<String, Object>();
+
+        requestBodyMap.put("dataSource", atlasDataSourceName);
+        requestBodyMap.put("database", atlasDatabaseName);
+        requestBodyMap.put("collection", atlasCollectionName);
+
+        HashMap<String, String> filter = new HashMap<String, String>();
+        filter.put("_id", idToGet);
+
+        requestBodyMap.put("filter", new HashMap<String, String>());
+
+        Request request = preparePostRequest(atlasCollectionName, "/action/findOne",
+                requestBodyMap);
+
+        try (Response response = client.newCall(request).execute()) {
+            JSONObject responseJson = new JSONObject(response.body().string());
+            try {
+                JSONObject itemDocument = responseJson.getJSONObject("document");
+                // General item attributes
+
+                String id = itemDocument.getString("_id");
+                String name = itemDocument.getString("name");
+                String description = itemDocument.getString("description");
+                String condition = itemDocument.getString("condition");
+                double price = itemDocument.getDouble("price");
+                int age = itemDocument.getInt("age");
+                boolean soldYet = itemDocument.getBoolean("soldYet");
+                String pickupAddress = itemDocument.getString("pickupAddress");
+                double radius = itemDocument.getDouble("radius");
+                // TODO: when we get around to this, we have to get a student based on
+                // the owner ID that is provided here like:
+                // Student.get(jsonDocument.getString("ownerId"));
+                Student owner = new Student("id", "test", "test", "test", "test", false,
+                        new ArrayList<>());
+                String type = itemDocument.getString("type");
+                String picture = itemDocument.getString("picture");
+                LocalDateTime creationTime =
+                    LocalDateTime.parse(itemDocument.getString("creationTime"));
+
+                // Item-specific attributes
+
+                String brand = itemDocument.getString("brand");
+                String colour = itemDocument.getString("colour");
+                ArrayList<String> capabilities = new ArrayList<>();
+
+                for (Object capability :
+                        itemDocument.getJSONArray("capabilities").toList()) {
+                    capabilities.add((String)capability);
+                        }
+
+                Technology newItem =
+                    new Technology(id, name, description, condition, price, age,
+                            soldYet, pickupAddress, radius, owner, type, picture,
+                            creationTime, brand, capabilities, colour);
+                return newItem;
+            } catch (JSONException e) { // this means it's null
+                return null;
+            }
         }
     }
 }
