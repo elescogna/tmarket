b4c7bc0744869bf613c4691389f3eb9f1b9445da
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2023-11-14 00:38:32 -0500

    refactor(generalize-data-access-interfaces): made data access interfaces inherit from general data access interface to prevent unnecessary parameters

---
+++ src/main/java/use_case/DataAccessInterface.java
@@ -0,0 +1,3 @@
+package use_case;
+
+public interface DataAccessInterface {}
--- src/main/java/use_case/home/HomeDataAccessInterface.java
+++ src/main/java/use_case/home/HomeDataAccessInterface.java
@@ -1,9 +1,11 @@
 package use_case.home;
 
 import entities.Item;
+import use_case.DataAccessInterface;
+
 import java.io.IOException;
 import java.util.ArrayList;
 
-public interface HomeDataAccessInterface {
+public interface HomeDataAccessInterface extends DataAccessInterface {
     ArrayList<Item> getAllItems() throws IOException;
 }
