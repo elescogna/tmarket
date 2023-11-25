b55bd77a37b216dffd62a3c3bad94a1ccefc1a2c
Your Name <avnia2705@gmail.com>
2023-11-24 19:00:56 -0500

    updated sign up use case

---
+++ src/main/java/app/SignupUseCaseFactory.java
@@ -0,0 +1,46 @@
+package app;
+
+import interface_adapter.login.LoginViewModel;
+import interface_adapter.signup.SignupController;
+import interface_adapter.signup.SignupPresenter;
+import interface_adapter.signup.SignupViewModel;
+import use_case.signup.SignupUserDataAccessInterface;
+import entities.CommonStudentFactory;
+import entities.StudentFactory;
+import interface_adapter.*;
+import use_case.signup.SignupInputBoundary;
+import use_case.signup.SignupInteractor;
+import use_case.signup.SignupOutputBoundary;
+import view.SignupView;
+
+import javax.swing.*;
+import java.io.IOException;
+public class SignupUseCaseFactory {
+    private SignupUseCaseFactory() {
+    }
+
+    public static SignupView create(
+            ViewManagerModel viewManagerModel, LoginViewModel loginViewModel, SignupViewModel signupViewModel, SignupUserDataAccessInterface userDataAccessObject) {
+
+        try {
+            SignupController signupController = createUserSignupUseCase(viewManagerModel, signupViewModel, loginViewModel, userDataAccessObject);
+            return new SignupView(signupController, signupViewModel);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Could not open user data file.");
+        }
+
+        return null;
+    }
+    private static SignupController createUserSignupUseCase(ViewManagerModel viewManagerModel, SignupViewModel signupViewModel, LoginViewModel loginViewModel, SignupUserDataAccessInterface userDataAccessObject) throws IOException {
+
+        // Notice how we pass this method's parameters to the Presenter.
+        SignupOutputBoundary signupOutputBoundary = new SignupPresenter(viewManagerModel, signupViewModel, loginViewModel);
+
+        StudentFactory userFactory = new CommonStudentFactory();
+
+        SignupInputBoundary userSignupInteractor = new SignupInteractor(
+                userDataAccessObject, signupOutputBoundary, userFactory);
+
+        return new SignupController(userSignupInteractor);
+    }
+}
